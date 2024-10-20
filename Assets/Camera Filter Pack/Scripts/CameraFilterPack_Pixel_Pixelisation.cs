31d2c72d789c22e9777b6ce8fbff351795ef1709
milenazxz <milena.ibragimova.02@mail.ru>
2024-10-20 20:28:50 +0300

    FilterBag

--- Assets/Camera Filter Pack/Scripts/CameraFilterPack_Pixel_Pixelisation.cs
+++ Assets/Camera Filter Pack/Scripts/CameraFilterPack_Pixel_Pixelisation.cs
@@ -1,6 +1,8 @@
+///////////////////////////////////////////
+//  CameraFilterPack - by VETASOFT 2018 ///
+///////////////////////////////////////////
 using UnityEngine;
 using System.Collections;
-
 [ExecuteInEditMode]
 [AddComponentMenu("Camera Filter Pack/Pixel/Pixelisation")]
 public class CameraFilterPack_Pixel_Pixelisation : MonoBehaviour
@@ -14,12 +16,7 @@ public class CameraFilterPack_Pixel_Pixelisation : MonoBehaviour
     [Range(0.6f, 120)]
     public float _SizeY = 1f;
     private Material SCMaterial;
-
-    public LayerMask excludeLayer; // Слой для объектов, которые не подвергаются пикселизации
-    private Camera mainCamera;
-    private RenderTexture nonPixelatedTexture;
     #endregion
-
     #region Properties
     Material material
     {
@@ -34,7 +31,6 @@ public class CameraFilterPack_Pixel_Pixelisation : MonoBehaviour
         }
     }
     #endregion
-
     void Start()
     {
         SCShader = Shader.Find("CameraFilterPack/Pixel_Pixelisation");
@@ -43,50 +39,22 @@ public class CameraFilterPack_Pixel_Pixelisation : MonoBehaviour
             enabled = false;
             return;
         }
-
-        mainCamera = GetComponent<Camera>();
-
-        // Создаём RenderTexture для объектов без фильтра
-        nonPixelatedTexture = new RenderTexture(Screen.width, Screen.height, 16);
     }
-
     void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
     {
         if (SCShader != null)
         {
-            // Сохраняем текущую маску слоёв камеры
-            int originalCullingMask = mainCamera.cullingMask;
-
-            // 1. Рендерим объекты из исключённого слоя в отдельную текстуру без применения фильтра
-            mainCamera.cullingMask = excludeLayer; // Рендерим только объекты из исключённого слоя
-            Graphics.Blit(sourceTexture, nonPixelatedTexture);
-
-            // 2. Применяем пикселизацию ко всему остальному
-            RenderTexture tempTexture = RenderTexture.GetTemporary(sourceTexture.width, sourceTexture.height);
-            mainCamera.cullingMask = originalCullingMask & ~excludeLayer; // Теперь рендерим всё, кроме исключённых объектов
             material.SetFloat("_Val", _Pixelisation);
             material.SetFloat("_Val2", _SizeX);
             material.SetFloat("_Val3", _SizeY);
-            Graphics.Blit(sourceTexture, tempTexture, material);
-
-            // 3. Накладываем исключённые объекты на пикселизированную сцену
-            Graphics.Blit(nonPixelatedTexture, tempTexture);
-
-            // 4. Выводим результат
-            Graphics.Blit(tempTexture, destTexture);
-
-            // Освобождаем временные ресурсы
-            RenderTexture.ReleaseTemporary(tempTexture);
-
-            // Восстанавливаем оригинальную маску слоёв камеры
-            mainCamera.cullingMask = originalCullingMask;
+            Graphics.Blit(sourceTexture, destTexture, material);
         }
         else
         {
             Graphics.Blit(sourceTexture, destTexture);
         }
     }
-
+    // Update is called once per frame
     void Update()
     {
 #if UNITY_EDITOR
@@ -96,18 +64,11 @@ public class CameraFilterPack_Pixel_Pixelisation : MonoBehaviour
         }
 #endif
     }
-
     void OnDisable()
     {
         if (SCMaterial)
         {
             DestroyImmediate(SCMaterial);
-        }
-
-        if (nonPixelatedTexture)
-        {
-            nonPixelatedTexture.Release();
-            DestroyImmediate(nonPixelatedTexture);
         }
     }
 }
